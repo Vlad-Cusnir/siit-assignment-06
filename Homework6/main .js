20d9400496a092632c77f92539eb97762307dff2
Vlad-Cusnir <vlad.cusnir@gmail.com>
2024-12-14 09:25:14 +0100

    move update

--- Homework6/main .js
+++ Homework6/main .js
@@ -57,36 +57,6 @@ function deleteBook(bookId){
     }
 
     const requestUrl = `${apiUrl}/books/${bookId}`
-    fetch(requestUrl, requestOptions)   // response is 204 no content, so the response does not need to be parsed
+    fetch(requestUrl, requestOptions)  
 }
 
-
-// CREATE
-/*
-const newBook = {
-    name: `Coraline`,
-    publish_date: `2002-07-02`
-}
-addBook(newBook);
-*/
-
-
-// READ
-// get the resource list (collection)
-getBooksList();
-
-// get the resource havig the id 1
-//getBook(1);
-
-
-// UPDATE
-/*
-const updBook = {
-    name: `Coraline11111`,
-    publish_date: `2002-07-07`
-}
-updateBook(11, updBook);
-*/
-
-// DELETE
-// deleteBook(11);
